8242795efe0721eb54939912cf4c8d0c402c8d14
Francesco Barbieri <checcobarbieri@gmail.com>
2022-01-13 13:52:46 +0100

    validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 
@@ -33,7 +34,12 @@ exports.getLogin = (req, res, next) => {
     isAuthenticated: false,
     //csrfToken: req.csrfToken(),
     errorMessage: mesErr,
-    infoMessage: mesInf
+    infoMessage: mesInf,
+    oldInput: {
+      email: '',
+      password: ''
+    },
+    validationErrors: []
   });
 };
 
@@ -47,18 +53,46 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    },
+    validationErrors: []
   });
 };
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password
+      },
+      validationErrors: errors.array()
+    });
+  }
+
   User.findOne({email: email})
     .then(user => {
-      if(!user){
-        req.flash('error', 'Email/password non valida.');
-        return res.redirect('/login');
+      if (!user) {
+        return res.status(422).render('auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage: 'Email/password non valida.',
+          oldInput: {
+            email: email,
+            password: password
+          },
+          validationErrors: []
+        });
       }
       bcrypt.compare(password, user.password)
         .then(doMatch => {
@@ -73,8 +107,16 @@ exports.postLogin = (req, res, next) => {
               return res.redirect(user.superuser ? '/admin/lessons' : '/');
             });
           }
-          req.flash('error', 'Password non valida.');
-          res.redirect('/login');
+          return res.status(422).render('auth/login', {
+            path: '/login',
+            pageTitle: 'Login',
+            errorMessage: 'Email/password non valida.',
+            oldInput: {
+              email: email,
+              password: password
+            },
+            validationErrors: []
+          });
         })
         .catch(err => {
           console.log(err);
@@ -88,39 +130,47 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const name = req.body.name;
-  const confirmPassword = req.body.confirmPassword;
-  User.findOne({email: email})
-    .then(userDoc => {
-      if(userDoc){
-        req.flash('error', 'Email risulta già registrata.');
-        return res.redirect('/signup');
-      }
-      return bcrypt.hash(password, 12)
-        .then(hashedPassword => {
-          const isSuperuser = constants.SUPERUSER_MAIL.find(val => val === email);
-          const user = new User({
-            email: email,
-            name: name,
-            superuser: isSuperuser ? true : undefined,
-            password: hashedPassword
-          });
-          return user.save();
-        })
-        .then(result => {
-          req.flash('info', 'Registrazione effettuata! Adesso puoi accedere con i tuoi dati. ');
-          res.redirect('/login');
-          return transporter.sendMail({
-            to: email,
-            from: 'checcobarbieri@gmail.com',
-            subject: 'Signup succeded!',
-            html: '<h1>You successfully signed up!</h1>'
-          });
-        })
-        .catch(err => {
-          console.log(err);
-        });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+        name:name,
+        confirmPassword: req.body.confirmPassword
+      },
+      validationErrors: errors.array()
+    });
+  }
+
+  bcrypt.hash(password, 12)
+    .then(hashedPassword => {
+      const isSuperuser = constants.SUPERUSER_MAIL.find(val => val === email);
+      const user = new User({
+        email: email,
+        name: name,
+        superuser: isSuperuser ? true : undefined,
+        password: hashedPassword
+      });
+      return user.save();
     })
-    .catch(err => {console.log(err)});
+    .then(result => {
+      req.flash('info', 'Registrazione effettuata! Adesso puoi accedere con i tuoi dati. ');
+      res.redirect('/login');
+      return transporter.sendMail({
+        to: email,
+        from: 'checcobarbieri@gmail.com',
+        subject: 'Signup succeded!',
+        html: '<h1>You successfully signed up!</h1>'
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.postLogout = (req, res, next) => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -8,9 +10,56 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post(
+    '/login', 
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Inserisci una mail valida.')
+            .normalizeEmail(),
+        body('password', 'La password deve essere di almeno 5 caratteri alfanumerici')
+            .isLength({ min: 5 })
+            .isAlphanumeric()
+            .trim()
+    ],
+    authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup', 
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Inserisci una mail valida.')
+            .custom((value, { req }) => {
+                // if (value === 'test@test.com') {
+                //   throw new Error('This email address if forbidden.');
+                // }
+                // return true;
+                return User.findOne({ email: value }).then(userDoc => {
+                if (userDoc) {
+                    return Promise.reject(
+                    'Indirizzo mail già presente, utilizza un\'altro.'
+                    );
+                }
+                });
+            })
+            .normalizeEmail(),
+        body(
+            'password',
+            'La password deve essere di almeno 5 caratteri alfanumerici.'
+            )
+            .isLength({ min: 5 })
+            .isAlphanumeric()
+            .trim(),
+        body('confirmPassword')
+            .trim()
+            .custom((value, { req }) => {
+                if (value !== req.body.password) {
+                throw new Error('La password non combacia.');
+                }
+                return true;
+            })
+    ],
+    authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
